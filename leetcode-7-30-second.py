0269ed53c825cc2aa1f3a0cc159cd0fc58d17668
liuyao734 <2698151329qq.com>
2024-07-30 22:47:25 +0800

    移除元素

---
+++ leetcode-7-30-second.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+            k = 0
+            for x in nums:
+                if x != val:
+                     nums[k] = x
+            return k
