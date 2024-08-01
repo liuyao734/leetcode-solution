70e7d43005661c3d11f031caa0600a9674a204be
liuyao734 <2698151329qq.com>
2024-08-01 17:29:34 +0800

    加一

---
+++ leetcode-8-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                for j in range(i + 1,n):
+                    dis[j] = 0
+                return digits
+        return [1] + [0] * n
