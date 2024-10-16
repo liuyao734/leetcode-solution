c378d5e3402fc8d33cf1810ff274601122c43a8f
liuyao734 <2698151329qq.com>
2024-10-16 22:55:59 +0800

    存在重复元素

---
+++ leetcode-10-16-second.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        s = set()
+        for i in range(n):
+            if i > k:
+                s.remove(nums[i-k-1])
+            if nums[i] in s:
+                return True
+            s.add(nums[i])
+        return False
